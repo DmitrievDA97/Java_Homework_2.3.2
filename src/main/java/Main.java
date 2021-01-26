1547be199380e7edbdf34d1784a520e0ac2ea414
Denis Dmitriev <denitos97@gmail.com>
2021-01-26 16:16:43 +0300

    fixes #1 code fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long bonus = service.calculate(amount, registered);
+    System.out.println(bonus);
   }
 }
 
